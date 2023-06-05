e1b373e7f5a0fb9cf23823a897cb774f63e3b2eb
Brenda <nikea@DESKTOP-7P7MJSP>
2023-06-05 19:17:01 -0300

    Add project files.

---
+++ ControleEstoque.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ControleEstoque.Web
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}"); //é usada para manipular solicitações de arquivos especiais, como arquivos de script WebResource.axd e ScriptResource.axd. Ignorar essa rota significa que o ASP.NET MVC não tentará rotear as solicitações para esse tipo de recurso
+
+            routes.MapRoute( // é usado para mapear uma URL para um determinado controlador e ação correspondente.
+               
+                name: "Default", //Isso define um nome para a rota. O nome "Default" é frequentemente usado para a rota padrão que é usada quando nenhuma outra rota é correspondente.
+               
+                url: "{controller}/{action}/{id}",//indica que a rota deve conter três segmentos: o nome do controlador, o nome da ação e um parâmetro opcional chamado "id". Esses segmentos são separados por barras ("/").
+               
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }//Essa parte define os valores padrão para o controlador, ação e parâmetro "id" caso eles não sejam fornecidos na URL.Neste caso, o controlador padrão é "Home",
+            //a ação padrão é "Index" e o parâmetro "id" é opcional.
+
+
+                );
+        }
+    }
+}
---
+++ ControleEstoque.Web/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace ControleEstoque.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas(); // Este método registra todas as áreas no aplicativo. As áreas são usadas para organizar e separar logicamente diferentes partes do aplicativo.
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);//Este método registra os filtros globais que serão aplicados a todas as solicitações do aplicativo. Os filtros globais são usados para adicionar comportamentos comuns a todas as ações dos controladores, como autenticação, autorização ou tratamento de erros.
+            RouteConfig.RegisterRoutes(RouteTable.Routes);//Este método registra as rotas para o aplicativo. As rotas definem como as URLs são mapeadas para os controladores e ações correspondentes.
+            BundleConfig.RegisterBundles(BundleTable.Bundles);//Este método registra os pacotes de recursos, conhecidos como bundles. Os bundles são grupos de arquivos CSS e JavaScript que são combinados e minificados para otimizar o desempenho do aplicativo.
+        }
+    }
+}
